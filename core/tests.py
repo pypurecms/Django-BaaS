aa67367c178ad91b1238cc9854802a53d33c8146
tim-hub <tim_w@outlook.com>
2019-04-04 14:46:32 +1300

    update test

--- core/tests.py
+++ core/tests.py
@@ -1,21 +1,22 @@
 from django.test import LiveServerTestCase
-from selenium import webdriver
+from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.common.keys import Keys
 
 
 class WebLiveTestCase(LiveServerTestCase):
 
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        self.browser = WebDriver()
         super(WebLiveTestCase, self).setUp()
 
     def tearDown(self):
         self.browser.quit()
         super(WebLiveTestCase, self).tearDown()
 
-    def test_register(self):
+    def test_admin(self):
         browser = self.browser
         # Opening the link we want to test
-        browser.get('http://localhost:8000')
+        print(self.live_server_url)
+        browser.get('{}/admin'.format(self.live_server_url))
         body = browser.find_element_by_tag_name('body')
-        assert 'Django' in body.text
+        assert 'Username' in body.text
