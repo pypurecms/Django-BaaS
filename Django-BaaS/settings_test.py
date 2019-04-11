962cac5958348a4b9995564336f39c639258b3fe
tim-hub <tim_w@outlook.com>
2019-04-12 11:45:07 +1200

    add db

--- Django-BaaS/settings_test.py
+++ Django-BaaS/settings_test.py
@@ -1,1 +1,11 @@
 from .settings_base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'circle_test',
+        'USER': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
+    }
+}
