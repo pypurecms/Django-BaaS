6ddb029427ea520c257301a7e9a3cb6d02e62afc
tim-hub <tim_w@outlook.com>
2019-04-16 17:03:05 +1200

    register core.models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Parent, Man, Woman, Child, Sibling
+
+admin.site.register(Parent)
+admin.site.register(Man)
+admin.site.register(Woman)
+admin.site.register(Child)
+admin.site.register(Sibling)
