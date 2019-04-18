aa542841c1a9bf725d6accf4229cb33ffa1cd0fa
tim-hub <tim_w@outlook.com>
2019-04-18 20:38:38 +1200

    add a basic test for all models

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -1,24 +1,34 @@
 from django.test import TestCase
-
-#Third-party app imports
 from model_mommy import mommy
-from model_mommy.recipe import Recipe, foreign_key
+from ..models import Human, Child, Parent, Sibling, Avatar, User
 
-# Relative imports of the 'app-name' package
-from core.models import Human, Child, Parent, Sibling, Avatar
 
-class ManTestCase(TestCase):
+class BaseModelTestCase(TestCase):
+    def setUp(self):
+        self.user1 = mommy.make(User, username='user1')
+        self.user2 = mommy.make(User, username='user2')
+
+        self.parent = mommy.make(Parent, name='Category1')
+        self.human = mommy.make(Human, user=self.user1, parent=self.parent)
+        self.childs = mommy.make(Child, name="comment", human=self.human, _quantity=3, user=self.user2)
+
+        ## make 5 siblings for the human
+        mommy.make(Sibling, _quantity=5, humans=[self.human.id])
+        self.avatar = mommy.make(Avatar, name='page', human=self.human)
+
+    def tearDown(self):
+        pass
+
+class ModelsBaseTestCase(BaseModelTestCase):
 
     def setUp(self):
-        self.human=mommy.make(Human)
-        self.childs = mommy.make(Child, human=self.human, _quantity=3)
-        self.parent = mommy.make(Parent, name='Category1')
-        mommy.make(Sibling, _quantity=5, humans=[self.human.id])
-        self.human.parent=self.parent
-        self.human.save()
+        super(ModelsBaseTestCase, self).setUp()
 
     def test_instance(self):
         self.assertTrue(isinstance(self.human, Human))
         self.assertEqual(self.childs[2].human.name, self.human.name)
         self.assertEqual(self.human.parent.name, 'Category1')
         self.assertEqual(len(self.human.siblings.all()), 5)
+        self.assertEqual(len(self.human.avatar.all()), 1)
+        self.assertEqual(self.human.user, self.user1)
+        self.assertEqual(self.childs[0].user, self.user2)
