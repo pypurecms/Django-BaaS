fb183afc05e7bc74d0952ad37cce83d34ebc1d7b
tim-hub <tim_w@outlook.com>
2019-04-26 12:44:12 +1200

    use DEBUG=true for now for serving static file

--- Django-BaaS/settings_heroku.py
+++ Django-BaaS/settings_heroku.py
@@ -8,13 +8,6 @@ ALLOWED_HOSTS = [
     '.herokuapp.com'
 ]
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'psql',
-#         'HOST': os.environ['DATABASE_URL'],
-#         'PORT': '5432'
-#     }
-# }
-
 DATABASES['default'] = dj_database_url.parse(os.environ['DATABASE_URL'], conn_max_age=600)
+
+
