fc4b2d59e15f3cdf459d615786d4379eed03cc61
tim-hub <tim_w@outlook.com>
2019-04-30 16:27:25 +1200

    load yaml to settings

--- Django-BaaS/settings_base.py
+++ Django-BaaS/settings_base.py
@@ -11,9 +11,16 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import ruamel.yaml as yaml
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+MODEL_DICT=yaml.safe_load(open('{}/config.yml'.format(BASE_DIR))).get('models')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
