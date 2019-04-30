01e9f5eb200a2c09c8fc25f77fc8fcbf50ba3afb
tim-hub <tim_w@outlook.com>
2019-04-30 21:12:41 +1200

    update heroku settings to force https

--- Django-BaaS/settings_heroku.py
+++ Django-BaaS/settings_heroku.py
@@ -4,15 +4,9 @@ import django_heroku
 from .settings_base import *
 
 DEBUG = True
-
-# ALLOWED_HOSTS = [
-#     '.herokuapp.com'
-# ]
-#
-# DATABASES['default'] = dj_database_url.parse(os.environ['DATABASE_URL'], conn_max_age=600)
-
-
-# Configure Django App for Heroku.
-
 django_heroku.settings(locals())
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
