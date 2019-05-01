44531f387ee719382017c5fac9b926e354b8d30a
tim-hub <tim_w@outlook.com>
2019-05-02 08:04:12 +1200

    Update settings to use customise templates

--- Django-BaaS/settings_base.py
+++ Django-BaaS/settings_base.py
@@ -60,7 +60,9 @@ ROOT_URLCONF = 'Django-BaaS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 # os.path.join(os.path.dirname(__file__), 'templates'),
+                 ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
