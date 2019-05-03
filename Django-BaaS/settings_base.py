a78613c3735659f1d7799e4d9386caf140f60bad
tim-hub <tim_w@outlook.com>
2019-05-03 15:26:56 +1200

    format settings

--- Django-BaaS/settings_base.py
+++ Django-BaaS/settings_base.py
@@ -63,7 +63,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'core/templates'),
-                 ],
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
