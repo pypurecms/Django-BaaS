df74ea585a677c260a6d2029f063b5db0b72fbe7
tim-hub <tim_w@outlook.com>
2019-05-03 16:22:42 +1200

    cover unenabled endpoint too

--- core/tests/test_unit.py
+++ core/tests/test_unit.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from model_mommy import mommy
 from ..models import Human, Child, Parent, Sibling, Avatar, User
+from ..utils import get_enabled
 
 
 class DummyTestCase(TestCase):
@@ -15,70 +16,85 @@ class DummyTestCase(TestCase):
 class HumanTestCase(APITestCase):
 
     def setUp(self):
+        self.endpoint_enabled = get_enabled('human')
         self.the_setup('humans', obj=Human)
 
     def tearDown(self):
         super(HumanTestCase, self).tearDown()
 
     def the_setup(self, base_name, obj):
-        self.base_name = base_name
-        self.password = 'password1'
-        self.user1 = mommy.make(User, username='user1')
-        self.user2 = mommy.make(User, username='user2')
-        self.user1.is_staff = True
-        self.user1.set_password(self.password)
-        self.user1.save()
-        self.objs = []
-        for i in range(3):
-            self.objs.append(mommy.make(obj, name='1-name-{}'.format(i), user=self.user1))
-        for i in range(2):
-            self.objs.append(mommy.make(obj, name='2-name-{}'.format(i), user=self.user2))
+        if self.endpoint_enabled:
+            self.base_name = base_name
+            self.password = 'password1'
+            self.user1 = mommy.make(User, username='user1')
+            self.user2 = mommy.make(User, username='user2')
+            self.user1.is_staff = True
+            self.user1.set_password(self.password)
+            self.user1.save()
+            self.objs = []
+            for i in range(3):
+                self.objs.append(mommy.make(obj, name='1-name-{}'.format(i), user=self.user1))
+            for i in range(2):
+                self.objs.append(mommy.make(obj, name='2-name-{}'.format(i), user=self.user2))
         super(HumanTestCase, self).setUp()
 
     def test_list_without_auth(self):
-        url = reverse('{}-list'.format(self.base_name))
-        response = self.client.get(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        if self.endpoint_enabled:
+            url = reverse('{}-list'.format(self.base_name))
+            response = self.client.get(url, format='json')
+            self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_list_with_staff_auth(self):
-        url = reverse('{}-list'.format(self.base_name))
-        self.client.login(username=self.user1.username, password=self.password)
-        response = self.client.get(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(3 + 2, response.data.get('count'), response.data)
-        self.client.logout()
+        if self.endpoint_enabled:
+            url = reverse('{}-list'.format(self.base_name))
+            self.client.login(username=self.user1.username, password=self.password)
+            response = self.client.get(url, format='json')
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(3 + 2, response.data.get('count'), response.data)
+            self.client.logout()
 
     def test_list_with_user_auth(self):
-        url = reverse('{}-list'.format(self.base_name))
-        self.user1.is_staff = False
-        self.user1.save()
-        self.client.login(username=self.user1.username, password=self.password)
-        response = self.client.get(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-        self.client.logout()
+        if self.endpoint_enabled:
+            url = reverse('{}-list'.format(self.base_name))
+            self.user1.is_staff = False
+            self.user1.save()
+            self.client.login(username=self.user1.username, password=self.password)
+            response = self.client.get(url, format='json')
+            self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+            self.client.logout()
 
     def test_list_mine_with_user_auth(self):
-        url = reverse('{}-list-mine'.format(self.base_name))
-        # function name list_mine but here it is list-mine
-        self.user1.is_staff = False
-        self.user1.save()
-        self.client.login(username=self.user1.username, password=self.password)
-        response = self.client.get(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(3, response.data.get('count'), response.data)
-        self.client.logout()
+        if self.endpoint_enabled:
+            url = reverse('{}-list-mine'.format(self.base_name))
+            # function name list_mine but here it is list-mine
+            self.user1.is_staff = False
+            self.user1.save()
+            self.client.login(username=self.user1.username, password=self.password)
+            response = self.client.get(url, format='json')
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(3, response.data.get('count'), response.data)
+            self.client.logout()
 
 
 class ChildTestCase(HumanTestCase):
     def setUp(self):
+        self.endpoint_enabled = get_enabled('child')
         self.the_setup('children', obj=Child)
 
 
 class SiblingTestCase(HumanTestCase):
     def setUp(self):
+        self.endpoint_enabled = get_enabled('sibling')
         self.the_setup('siblings', obj=Sibling)
 
 
 class ParentTestCase(HumanTestCase):
     def setUp(self):
+        self.endpoint_enabled = get_enabled('parent')
         self.the_setup('parents', obj=Parent)
+
+
+class AvatarTestCase(HumanTestCase):
+    def setUp(self):
+        self.endpoint_enabled = get_enabled('avatar')
+        self.the_setup('avatars', obj=Avatar)
