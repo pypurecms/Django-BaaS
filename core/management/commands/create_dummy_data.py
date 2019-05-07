775d170090492f7459eeab1b3a29bf62a518d0b8
tim-hub <tim_w@outlook.com>
2019-05-07 17:26:54 +1200

    use create dummy data in heroku

--- core/management/commands/create_dummy_data.py
+++ core/management/commands/create_dummy_data.py
@@ -18,36 +18,38 @@ class Command(BaseCommand):
     help = 'Create Dummy Data.'
 
     def handle(self, *args, **kwargs):
-        parents = []
-        siblings = []
-        users = []
-        humans = []
+        if Human.objects.all().count() < 5:
+            parents = []
+            siblings = []
+            users = []
+            humans = []
+            for i in range(3):
+                user = mommy.make(User, username=fake.user_name(), email=fake.email(), password=fake.sentence(nb_words=2) )
+                users.append(user)
 
-        for i in range(3):
-            user = mommy.make(User, username=fake.user_name(), email=fake.email(), password=fake.sentence(nb_words=2) )
-            users.append(user)
+            for i in range(5):
+                parent = mommy.make(Parent, user=random_pick(users), name=fake.word(), description=fake.sentence())
+                sibling = mommy.make(Sibling, user=random_pick(users), name= fake.word(), description=fake.sentence())
+                parents.append(parent)
+                siblings.append(sibling)
 
-        for i in range(5):
-            parent = mommy.make(Parent, user=random_pick(users), name=fake.word(), description=fake.sentence())
-            sibling = mommy.make(Sibling, user=random_pick(users), name= fake.word(), description=fake.sentence())
-            parents.append(parent)
-            siblings.append(sibling)
+            for i in range(10):
+                human = mommy.make(Human,
+                                   user=random_pick(users),
+                                   name= fake.sentence(nb_words=3),
+                                   content= fake.text(max_nb_chars=200, ext_word_list=None),
+                                   parent=random_pick(parents),
+                                   siblings=random.sample(siblings, 2)
+                                   )
+                humans.append(human)
 
-        for i in range(10):
-            human = mommy.make(Human,
-                               user=random_pick(users),
-                               name= fake.sentence(nb_words=3),
-                               content= fake.text(max_nb_chars=200, ext_word_list=None),
-                               parent=random_pick(parents),
-                               siblings=random.sample(siblings, 2)
-                               )
-            humans.append(human)
-
-        for i in range(20):
-            mommy.make(Child,
-                       user=random_pick(users),
-                       name = fake.sentence(nb_words=3),
-                       content = fake.text(max_nb_chars=200, ext_word_list=None),
-                       human = random_pick(humans)
-                       )
+            for i in range(20):
+                mommy.make(Child,
+                           user=random_pick(users),
+                           name = fake.sentence(nb_words=3),
+                           content = fake.text(max_nb_chars=200, ext_word_list=None),
+                           human = random_pick(humans)
+                           )
+        else:
+            print('Do not need to create dummy data.')
 
